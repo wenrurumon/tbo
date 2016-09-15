ed712934372686608df8d9db07fc8924c56eed01
Wenrurumon <askahu6@gmail.com>
2016-09-15 11:06:01 -0500

    Update and rename matrix_completion.R to data_transformation.R

--- data_transformation.R
+++ data_transformation.R
@@ -93,4 +93,22 @@ mc0 <- function(X,lambda=0.2,ifprint=FALSE){
   rlt <- list(Z = A%*%Z, X=A, Y=Z ,prop=cumsum(svd(X-E)$d/sum(svd(X-E)$d)))
   return(rlt)  
 }
+#Functional Kernal Representation
+library(fda)
+library(flare)
+functional_transform <- function(X){
+  #calculate the distance for each variables
+  X.dist <- dist(t(X))
+  pos <- hclust(X.dist)$order
+  X <- X[,pos]
+  X.dist <- as.matrix(X.dist)[pos,pos]
+  pos <- c(0,cumsum(diag(X.dist[-1,-length(pos)])))
+  pos <- scale0(pos,T,T)
+  #set fourier basis
+  fbasis<-create.fourier.basis(c(0,1),nbasis=length(pos)*2-1)
+  fphi <- eval.basis(pos,fbasis)
+  fcoef <- ginv(t(fphi)%*%fphi)%*%t(fphi)%*%t(X)
+  rlt <- t(fcoef-rowMeans(fcoef))/sqrt(nrow(X))
+  return(rlt)
+}
 
