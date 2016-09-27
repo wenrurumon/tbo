d653c39d7b9eb91617b8322bb18ba9473539ce63
Wenrurumon <askahu6@gmail.com>
2016-09-27 15:50:11 -0500

    Update build_undirected_network.R

--- network_analysis/build_undirected_network.R
+++ network_analysis/build_undirected_network.R
@@ -36,27 +36,18 @@ sem_l1 <- function(y,prop,lambda,times=1){
   Y.prop <- do.call(c,lapply(y,function(x){diff(c(0,x$prop[1:which(x$prop>=prop)[1]]))}))
   Y.group <- rep(1:length(y),sapply(y,function(x){which(x$prop>=prop)[1]}))
   #Lasso Network
-  if(times==1){
-    adj <- lapply(1:ncol(Y),function(i){
+  adjs <- lapply(1:times,function(i){
+    Y <- Y[sample(1:nrow(Y),nrow(Y)/2),]
+    adj <- do.call(rbind,lapply(1:ncol(Y),function(i){
       slimi <- slim(X=Y[,-i],Y=Y[,i],lambda=lambda,method='lasso',verbose=FALSE)
       temp <- rep(FALSE,ncol(Y))
       temp[-i][which(slimi$beta!=0)] <- TRUE
       temp
-    })
-  } else {
-    adjs <- lapply(1:times,function(i){
-      Y <- Y[sample(1:nrow(Y),nrow(Y)/2),]
-      adj <- do.call(rbind,lapply(1:ncol(Y),function(i){
-        slimi <- slim(X=Y[,-i],Y=Y[,i],lambda=lambda,method='lasso',verbose=FALSE)
-        temp <- rep(FALSE,ncol(Y))
-        temp[-i][which(slimi$beta!=0)] <- TRUE
-        temp
-      }))
-    })
-    adj <- 0
-    for(i in adjs){adj <- i+adj}
-    adj <- lapply(1:nrow(adj),function(i){adj[i,]>=0.8})
-  }
+    }))
+  })
+  adj <- 0
+  for(i in adjs){adj <- i+adj}
+  adj <- lapply(1:nrow(adj),function(i){adj[i,]>=0.8})
   #Parameter Estimation
   model <- do.call(rbind,lapply(1:length(adj),function(i){
     yi <- Y[,adj[[i]],drop=F]
@@ -71,8 +62,14 @@ sem_l1 <- function(y,prop,lambda,times=1){
                coef=as.vector(yi.coef),pvalue=yi.pvalue)
   }))
   #Summarise the model into group
-  adj2 <- sapply(adj,function(x){tapply(x,Y.group,sum)>0})
-  adj2 <- t(sapply(1:nrow(adj2),function(i){tapply(adj2[i,],Y.group,sum)>0}))
+  # adj2 <- sapply(adj,function(x){tapply(x,Y.group,sum)>0})
+  # adj2 <- t(sapply(1:nrow(adj2),function(i){tapply(adj2[i,],Y.group,sum)>0}))
+  adj2 <- sapply(1:length(adj),function(i){
+    tapply(ifelse(adj[[i]]!=0,Y.prop,0),Y.group,sum)
+  })
+  adj2 <- t(sapply(1:nrow(adj2),function(i){
+    tapply(adj2[i,] * Y.prop,Y.group,sum)
+  }))
   model2 <- model %>% 
     group_by(y.group,x.group) %>% 
     summarise(y.prop=sum(y.prop),x.prop=sum(x.prop),coef=sum(y.prop*x.prop*coef),pvalue=mean(pvalue))
@@ -87,8 +84,9 @@ sem_grplasso <- function(y,prop=0.8,lambda=1,times=1){
   Y.prop <- do.call(c,lapply(y,function(x){diff(c(0,x$prop[1:which(x$prop>=prop)[1]]))}))
   Y.group <- rep(1:length(y),sapply(y,function(x){which(x$prop>=prop)[1]}))
   #Group Lasso Network
-  if(times==1){
-    adj <- lapply(1:ncol(Y),function(i){
+  adjs <- lapply(1:times,function(i){
+    Y <- Y[sample(1:nrow(Y),nrow(Y)/2),]
+    adj <- do.call(rbind,lapply(1:ncol(Y),function(i){
       lambda <- lambdamax(x=cbind(1,Y[,-i]),y=Y[,i], 
                           index=c(NA,Y.group[-i]), 
                           penscale = sqrt, model = LinReg(),
@@ -100,28 +98,11 @@ sem_grplasso <- function(y,prop=0.8,lambda=1,times=1){
       temp <- rep(0,ncol(Y))
       temp[-i] <- coef(fit)[-1]
       temp
-    })
-  } else {
-    adjs <- lapply(1:times,function(i){
-      Y <- Y[sample(1:nrow(Y),nrow(Y)/2),]
-      adj <- do.call(rbind,lapply(1:ncol(Y),function(i){
-        lambda <- lambdamax(x=cbind(1,Y[,-i]),y=Y[,i], 
-                            index=c(NA,Y.group[-i]), 
-                            penscale = sqrt, model = LinReg(),
-                            center=TRUE,standardized=TRUE) * 0.5^(1/lambda-1)
-        fit <- grplasso(x=cbind(1,Y[,-i]),y=Y[,i],
-                        index=c(NA,Y.group[-i]),lambda=lambda,model=LinReg(),
-                        penscale = sqrt,
-                        control = grpl.control(update.hess = "lambda", trace = 0))
-        temp <- rep(0,ncol(Y))
-        temp[-i] <- coef(fit)[-1]
-        temp
-      }))
-    })
-    adj <- 0
-    for(i in adjs){adj <- (i!=0)+adj}
-    adj <- lapply(1:nrow(adj),function(i){adj[i,]>=0.8})
-  }
+    }))
+  })
+  adj <- 0
+  for(i in adjs){adj <- (i!=0)+adj}
+  adj <- lapply(1:nrow(adj),function(i){adj[i,]>=0.8})
   #Summarise network
   adj2 <- sapply(1:length(adj),function(i){
     tapply(ifelse(adj[[i]]!=0,Y.prop,0),Y.group,sum)
@@ -138,4 +119,5 @@ sem_grplasso <- function(y,prop=0.8,lambda=1,times=1){
 #####################################################
 
 system.time(test1 <- sem_l1(y=input$y,prop=.8,lambda=.5,times=100))
-system.time(test2 <- sem_grplasso(y=input$y,prop=.8,lambda=1,times=100))
+system.time(test2 <- sem_grplasso(y=input$y,prop=.8,lambda=.5,times=1))
+plotnet(test2$network2>0.4)
