b2f36faaca4d06b6af85bc28e84c6ed393cf06e8
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-07 15:29:41 -0500

    Create unix.sh

---
+++ training_rita/unix.sh
@@ -0,0 +1,23 @@
+	jianis1@low1$ pwd
+	/zfsauton/project/highmark/aska/training_rita
+	jianis1@low1$ ls
+	rawdata
+	jianis1@low1$ ls -l
+	total 19
+	drwxrwxr-x. 2 jianis1 highmark 12 Jun  7 16:26 rawdata
+	jianis1@low1$ cd rawdata
+	jianis1@low1$ pwd
+	/zfsauton/project/highmark/aska/training_rita/rawdata
+	jianis1@low1$ ls -l
+	total 113815
+	-rw-rw-r--. 1 jianis1 highmark  13743752 Jun  7 16:26 0.txt
+	-rw-rw-r--. 1 jianis1 highmark   2914666 Jun  7 16:26 1.txt
+	-rw-rw-r--. 1 jianis1 highmark   1674004 Jun  7 16:26 2.txt
+	-rw-rw-r--. 1 jianis1 highmark    926474 Jun  7 16:26 3.txt
+	-rw-rw-r--. 1 jianis1 highmark    691677 Jun  7 16:26 4.txt
+	-rw-rw-r--. 1 jianis1 highmark   1770113 Jun  7 16:26 5.txt
+	-rw-rw-r--. 1 jianis1 highmark   3960956 Jun  7 16:26 6.txt
+	-rw-rw-r--. 1 jianis1 highmark 131590866 Jun  7 16:26 7.txt
+	-rw-rw-r--. 1 jianis1 highmark 217110116 Jun  7 16:26 8.txt
+	-rw-rw-r--. 1 jianis1 highmark 373022618 Jun  7 16:27 9.txt
+	jianis1@low1$ 
