c912f642c6b8df807b8a56e346b62bf5cefa0b0f
進撃の雯雯老師 <askahu6@gmail.com>
2018-10-17 21:47:34 +0800

    Update fbd_monday.R

--- fbd_monday.R
+++ fbd_monday.R
@@ -2,8 +2,14 @@
 rm(list=ls())
 setwd('E:\\gsk\\fbd_flx\\model')
 # raw <- openxlsx::read.xlsx('mmix_fbd.xlsx',1)[,-1]
-raw <- data.table::fread('mmix_fbd_withsku.csv')
-raw <- as.data.frame(raw)[,-1]
+raw <- as.data.frame(data.table::fread('mmix_fbd_withsku.csv'))[,-1]
+raw2 <- as.data.frame(data.table::fread('mmix_fbd_sku2.csv'))[,-1]
+pcat1.val <- (raw2[,colnames(raw2)%in%c(grep('Val.sku',grep('GeneralPainRelief',colnames(raw2),value=T),value=T))])
+pcat2.val <- (raw2[,colnames(raw2)%in%c(grep('Val.sku',grep('MuscularPainRelief',colnames(raw2),value=T),value=T))])
+pcat3.val <- (raw2[,colnames(raw2)%in%c(grep('Val.sku',grep('100_Pain',colnames(raw2),value=T),value=T))])
+pcat.val <- rowSums(pcat1.val)+rowSums(pcat2.val)+rowSums(pcat3.val)
+tcm.val <- rowSums(cbind(pcat1.val,pcat2.val,pcat3.val)[,grep('Tcm',colnames(cbind(pcat1.val,pcat2.val,pcat3.val)))])
+getvar('OTC')
 
 #############################
 # Function
@@ -80,9 +86,9 @@ compare <- function(x){
 ydum <- do.call(rbind,strsplit(paste(raw$Month),'/'))[,1]
 cdum <- raw$City
 mdum <- paste0('M',rep(rep(1:12,length=30),length=810))
-y1 <- ydum=='2016'
-y2 <- ydum=='2017'
-y3 <- ydum=='2018'
+y1 <- as.numeric(ydum=='2016')
+y2 <- as.numeric(ydum=='2017')
+y3 <- as.numeric(ydum=='2018')
 
 #sales data
 fbd.val <- getvar('FENBID_VAL.sales|FENBID_FAST_VAL.sales',F,T)
@@ -118,23 +124,31 @@ fbd300.val <- fbd.val - fbd400.val
 fbd300.vol <- fbd.vol - fbd400.vol
 fbd300.avp <- fbd300.val / fbd300.vol
 fbd400.avp <- fbd400.val / fbd400.vol
-hh <- fbd.vol / mc(fbd.vol)
+
 
 #ATL
 fbd.tv <- ret(getvar('GRP.tv',F,F)[,1],0.3)
+fbd.tv1 <- ret(getvar('GRP.tv',F,F)[,1]*y1,0.3)
+fbd.tv2 <- ret(getvar('GRP.tv',F,F)[,1]*y2,0.3)
+fbd.tv3 <- ret(getvar('GRP.tv',F,F)[,1]*y3,0.3)
+
+
 fbd.otv <- ret(getvar('PC_GRP.otv|Mobile_GRP.otv',F,T),0.3)
 fbd.semclick <- ret(getvar('Clicks.sem',F,T),0.3)
 fbd.semimp <- ret(getvar('Impressions.sem',F,T),0.3)
-fbd.digital <- ret(getvar('Impression.digital',F,T),0.3)
-fbd.social <- getvar('Impressions.social|Impressions_.social',F,F)[,2]
-fbd.social[fbd.social==59597928] <- sum(c(1474320,112301))
-fbd.social <- ret(fbd.social,0.3)
+fbd.digital <- ret(rowSums(getvar('Impression.digital',F,F)[,-5]),0.3)
+fbd.digital_social <- ret(getvar('Impression.digital',F,F)[,5],0.3)
+fbd.social <- ret(getvar('Impressions.social|Impressions_.social',F,T),0.3)
+# fbd.social[fbd.social==59597928] <- sum(c(1474320,112301))
+# fbd.social <- ret(fbd.social,0.3)
 
 #BTL
 fbd.posm <- getvar('hejiyujidanpinshuliang.posm',F,T)
+fbd.posm1 <- rep(1:30,27)%in%(5:12)
 fbd.rmd <- getvar('hejiyujidanpinshuliang.rmd',F,T)
 fbd.edu <- getvar('shijirenshu.edu',F,T)
 fbd.app <- getvar('saomaliang.app',F,T)
+
 
 #############################
 # Modeling
@@ -142,26 +156,49 @@ fbd.app <- getvar('saomaliang.app',F,T)
 
 # reprocessing
 y.vol <- fbd.vol
+hh <- y.vol / mc(y.vol)
 y.vol2 <- y.vol / hh
 holdout <-   cbind(
   intercept = rep(0,810)
-  ,fbd.tv = fbd.tv*9.826528e-05*2
+  ,pcat.val = pcat.val*6.39825e-05
+  ,fbd400.vol = mc(fbd400.vol)*0.144583642
+  ,fbd.tv=fbd.tv1*9.826528e-05*2
+  +fbd.tv2*9.826528e-05*2
+  +fbd.tv3*9.826528e-05*2
+  ,fbd.otv=fbd.otv*5.890516e-04*0.7
+  ,fbd.sem=fbd.semclick*4.911940e-06/3
+  ,fbd.digital=fbd.digital*6.815926e-12+fbd.digital_social*4.007322e-11*4
+  ,fbd.social = fbd.social*4.007322e-11*4
+  ,fbd.posm = fbd.posm1*0.0164719+fbd.posm*5.052831e-06/2
+  ,fbd.rmd = fbd.rmd*7.381199e-06
+  ,fbd.app = fbd.app*1.007945e-06
+  ,fbd.edu = fbd.edu*9.573258e-04*0.02013463
 )
 y.vol3 <- y.vol2 - rowSums(holdout)
 
 # Model
 xlm <- lm(y.vol3 ~ -1
-          + paste(cdum,mdum)
-          + fbd.wd + fbd.avp
-          # + mc(fbd300.vol)
+          + cdum
+          +paste(cdum,mdum)
+          +mc(fbd400.wd)
+          # +y1+y2+y3
+          # +fbd.tv1+fbd.tv2+fbd.tv3
 )
 tail(coef(summary(xlm)))
-plot.model(mt(y.vol),mt((predict(xlm)+rowSums(holdout))*hh),min(mt(y.vol))*0.7)
+plot.model(mt(y.vol),mt((predict(xlm)+rowSums(holdout))*hh),min(mt(y.vol))*0.5)
 
 # Calc
+check((mc((fbd400.wd)^1/3)*0.5292653+y1*0.09944523+y2*0.29267172+y3*0.40809198)*hh,fbd400.vol)
 check(fbd400.eqhh*0.183083943*hh,fbd.vol)
 check(fbd400.wd*0.0009729*hh,fbd400.vol)
 check(fbd.tv*9.826528e-05*2*hh,fbd.vol)
+check(fbd.otv*5.890516e-04*0.7*hh,fbd.vol)
+check(fbd.semclick*4.911940e-06/3*hh,fbd.vol)
+check(fbd.digital*6.815926e-12*hh,fbd.vol)
+check(fbd.social*4.007322e-11*4*hh,fbd.vol)
+check(fbd.edu*9.573258e-04*0.02013463*hh,fbd.vol)
+check(fbd.posm1*0.0164719*hh,fbd.vol)
+check(pcat.val*6.39825e-05*hh,fbd.vol)
 
 # Tech Review
 y.raw <- mt(y.vol)
@@ -169,41 +206,3 @@ y.pred <- mt((predict(xlm)+rowSums(holdout))*hh)
 plot.model(y.raw,y.pred)
 summary(abs(y.pred/y.raw-1),2)
 
-#############################
-# Decomp
-#############################
-
-decomp <- cbind(
-  fbd.wd = mc(fbd.wd)*0.44999799
-  ,fbd400.wd = mc(fbd400.wd)*-0.15238009
-  ,fbd.avp = fbd.avp*-1.22536989
-  ,fbd400.vol = fbd400.eqhh*0.120199506
-  ,fbd.tv = fbd.tv*9.826528e-05*2
-  ,fbd.otv = fbd.otv*5.890516e-04*0.7
-  ,fbd.sem = fbd.semclick*4.911940e-06/3
-  ,fbd.digital = fbd.digital*6.815926e-12
-  ,fbd.social = fbd.social*4.007322e-9*4
-  ,fbd.posm = fbd.posm*5.052831e-06/2
-  ,fbd.rmd = fbd.rmd*7.381199e-06
-  ,fbd.app = fbd.app*1.007945e-06
-  ,fbd.edu = fbd.edu*9.573258e-04*0.02013463
-)
-decomp <- apply(decomp,2,function(x){
-  mt(x*hh)
-})
-y.res <- mt(y.vol) - rowSums(decomp)
-y.season <- predict(lm(y.res ~ paste(rep(1:12,length=30))-1))
-decomp <- cbind(decomp,season = y.season)
-decomp <- data.frame(decomp,predict=rowSums(decomp),actual=mt(y.vol),value=mt(fbd.val))
-rownames(decomp) <- unique(raw$Month)
-plot.model(decomp$actual,decomp$predict,min(decomp$predict)*0.5)
-
-decomp <- rbind(decomp,
-                apply(decomp,2,function(x){
-                  out1 <- tapply(x,rep(1:3,each=12)[1:30],sum)
-                  out2 <- tapply(x,c(rep(0,6),rep(4:5,each=12)),sum)
-                  c(out1,out2[-1])
-                })
-)
-
-# write.csv(t(decomp),'decomp_fbd.csv')
